d08ef3a19939067f9eb95213920e18ae5edc382c
Nabin <nabinbhatta@my.unt.edu>
2021-02-12 14:10:09 -0600

    modified search bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Navbar, Nav, NavDropdown, Button, Form, FormControl,} from "react-bootstrap";
+import { Navbar, Nav, NavDropdown} from "react-bootstrap";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Makeacc from './pages/makeacc';
 import Home from './pages/home';
@@ -11,40 +11,42 @@ function App() {
     <Router>
 
     <>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar collapseOnSelect expand="lg"  variant="dark" className="nav">
         <Navbar.Brand href="/">Logo</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" className="navbar" />
+        <Navbar.Collapse id="responsive-navbar-nav" className="nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#features">About</Nav.Link>
-
-            <Button variant="outline-info" href="#features">
-              Monthly Winner
-            </Button>
-            {/* <Nav.Link  href="#features">Featured Channels</Nav.Link> */}
-            <Nav.Link href="#talk">Talk</Nav.Link>
-            <NavDropdown title="Genre" id="collasible-nav-dropdown">
+            <Nav.Link href="#features"className="nav">About</Nav.Link>
+          
+           
+            <Nav.Link  className="nav btn-nav" href="#winner">Featured Channels</Nav.Link>
+            <Nav.Link href="#talk" className="nav">Talk</Nav.Link>
+            <NavDropdown title="Genre" id="collasible-nav-dropdown" className="nav">
               <NavDropdown.Item href="#action/3.1">Sports</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Horror</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Comedy</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Free-style</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4" >Free-style</NavDropdown.Item>
             </NavDropdown>
-            <Form inline>
+            <form >
+              <div className="container">
+  <input href="/searchengine" type="text" placeholder="Search..."/>
+  <div className="search"></div>
+  </div>
+        </form>
+            {/* <Form inline className="nav" >
               <FormControl
                 type="text"
                 placeholder="Search..."
-                className="mr-sm-2 ml-xl-5"
+                className="mr-sm-2 ml-xl-5 "
               />
-              <Button variant="outline-success">Search</Button>
-            </Form>
+              <Button variant="outline-success" className="">Search</Button>
+            </Form> */}
           </Nav>
           <Nav>
-            <Button variant="outline-info" href="/signup">
-              SIGN UP
-            </Button>
-            {/*  <Nav.Link href="#make_account">SIGN UP</Nav.Link> */}
-            <Nav.Link eventKey={2} href="#login" className="">
+           
+             <Nav.Link href="/signup" className="nav btn-nav">SIGN UP</Nav.Link>
+            <Nav.Link /* eventKey={2} */ href="#login" className="nav">
               SIGN IN
             </Nav.Link>
           </Nav>
