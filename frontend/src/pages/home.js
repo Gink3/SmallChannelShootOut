22814a6f2668d5059ca37b8283643063cd519b0e
Nabin <nabinbhatta@my.unt.edu>
2021-02-19 23:06:10 -0600

    homepage for presentation

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,9 +1,39 @@
 import React from 'react';
+//import { Button } from 'react-bootstrap';
+import './pages.scss';
+import starIcon from "../images/star-like.svg";
+
+
+const videobox=["First", "Second", "Third", "Fourth", "Fifth","Sixth", "Seventh", "Eighth", "Ninth", "Tenth", "Eleventh"];
+const videoboxObj= videobox.map((videobox, i)=>({id: i, title:videobox}));
+
+function Intro(props) {
+  return (
+    <section>
+      <ul>
+        {props.videoboxes.map((videobox) => (
+          
+          <li key={videobox.id}>{videobox.title}
+  
+          <button className="like">
+          <img src={starIcon} alt="star icon" width="30px" className="like-icon"/>
+            </button>
+          
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
 
 function home(){
     return(
         <>
-            HOMEPAGE
+       
+            <div className="homebox">
+            <Intro videoboxes={videoboxObj} />
+                
+            </div>
         </>
     );
 }
