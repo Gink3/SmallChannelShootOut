5e4cdfbc6e110a5935a3255f396f33d704404171
Tyler Parks <tylerparks@my.unt.edu>
2021-02-24 11:08:44 -0600

    Simple "Talk" page

--- frontend/src/pages/talk.js
+++ frontend/src/pages/talk.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import { Button, Card,  Container, Row, Col, CardColumns } from "react-bootstrap";
+import "../style/talk.scss";
 
 function talk(){
     return(            
         <>
-            TALK
+
+            <div className="page-manager">
+                Talk
+
+                <Card>
+                    <Card.Body>
+                        <Card.Text className="text-muted">
+                            Posted by User123
+                        </Card.Text>
+
+                        <Card.Title>Awesome Thread!</Card.Title>
+
+                        <Button variant="primary">Comments</Button>
+                        <Button variant="primary">Share</Button>
+                        <Button variant="primary">Save</Button>
+                        <Button variant="primary">...</Button>
+                    </Card.Body>
+                </Card>
+
+                <Card>
+                    <Card.Body>
+                        <Card.Text className="text-muted">
+                            Posted by User045
+                        </Card.Text>
+
+                        <Card.Title>Crazy video by this youtuber</Card.Title>
+
+                        <Button variant="primary">Comments</Button>
+                        <Button variant="primary">Share</Button>
+                        <Button variant="primary">Save</Button>
+                        <Button variant="primary">...</Button>
+                    </Card.Body>
+                </Card>
+            </div>
         </>    
     );
 }
