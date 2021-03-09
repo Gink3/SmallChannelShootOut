e5d4f0de8e2aa11ddf493d31d38b0dade9bbd174
Saiman Sigdel <saimansigdel@my.unt.edu>
2021-03-09 12:50:58 -0600

    remove unnecessary file

--- backend/route/SignupRoute.js
+++ backend/route/SignupRoute.js
@@ -1,9 +1,20 @@
 import express from 'express'
+import bcrypt from 'bcrypt';
+import User from '../models/UserModel.js'
 
 const router = express.Router(); 
 
-import {signUp} from '../Controllers/Signupcontrol.js'
 
-export const signUpRouter = router.post('/signup', signUp); 
+export const signUpRouter = router.post('/signup', (req, res) => {
+   var {firstName, lastName, userName, email, password} = req.body; 
+    password = bcrypt.hashSync(password, 12);
+    try {
+        const newUser = new User({firstName, lastName, userName, email, password}); 
+        newUser.save(); 
+    }
+    catch(error) {
+        res.status(409).json({message: error.message});
+    }
+}); 
 
 
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose'; 
 import {signUpRouter} from './route/SignupRoute.js'
-const ATLAS_URI= "mongodb+srv://admin:SmallChannelSh00tOut!@cluster0.q9opq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+import dotenv from 'dotenv'; 
 
+dotenv.config(); 
 
 const app = express();
 const port = process.env.PORT || 5000; 
@@ -11,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(cors()); 
 app.use(express.json()); 
 
-const uri = ATLAS_URI; //uri for mongodb
+const uri = process.env.ATLAS_URI; //uri for mongodb
 
 mongoose.connect (uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
