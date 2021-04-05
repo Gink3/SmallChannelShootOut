d74bae580eb2dd2308459aaf2f50c91a89b75489
Nabin <nabinbhatta@my.unt.edu>
2021-04-05 18:04:05 -0500

    little changes of adsense

--- frontend/src/pages/talk.js
+++ frontend/src/pages/talk.js
@@ -4,6 +4,7 @@ import "../style/talk.scss";
 import {AiOutlinePlus} from "react-icons/ai";
 import {FiThumbsDown, FiThumbsUp} from "react-icons/fi";
 import toast from "../images/toast.png";
+ /* import AdSense from 'react-adsense';  */
 
 const threadbox=[
     {title:"Thread 1", user:"User01", timestamp:"just now", likes:"62"},
@@ -68,53 +69,18 @@ sdLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         {/*   <Button variant=""> */}
             <FiThumbsDown className="dislike-btn" size="1.5em" color="red"/>
-          {/*   </Button> */}
-        </Card.Body>
-      </Card>
-       /* <Toast className="backside" closeButton='false'>
-          <Toast.Header> */
-
-          /*   Likes */
-           /*  <FiThumbsUp className="like-btn" size="1.25em" color="green"/>
-
             &nbsp;
             {thread.likes}
-            &nbsp;
+          {/*   </Button> */}
 
-            <FiThumbsDown className="dislike-btn" size="1.25em" color="red"/>
- */
-            /*Profile Picture*/
-           /*  &nbsp;&nbsp;&nbsp;&nbsp;
-            <img src={toast} className="rounded mr-2"/> */
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <Button className="btn-comment" variant="" >
+            Comments
+            </Button>
 
-            /*Username*/
-           /*  <strong className="mr-auto">
-              {thread.user}      
-            </strong> */
-
-            /*Title*/
-            /* <medium className="mr-auto">
-              {thread.title}
-              &nbsp;&nbsp;&nbsp;&nbsp;
-              &nbsp;&nbsp;&nbsp;&nbsp;
-              &nbsp;&nbsp;&nbsp;&nbsp;
-            </medium>
-             */
-            /*Timestamp*/
-           /*  <small>
-              {thread.timestamp}
-            </small>
-          </Toast.Header>
- */
-          /*Message*/
-          /* <Toast.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </Toast.Body>
-        </Toast> */
-          
+        </Card.Body>
+      </Card>
+                
       ))}
     </>
   );
@@ -126,7 +92,7 @@ function talk(){
       <Container fluid>
         <br/>
 
-        <Button className="plus"/*  style={{ marginBottom: "2px", marginTop: "2px",  color:'white', fontSize: '20px'}} */ variant="" href="/createthread"  >
+        <Button className="plus" variant="" href="/createthread"  >
           <AiOutlinePlus size="3em"/>Create a thread!
         </Button>
 
@@ -144,9 +110,13 @@ function talk(){
   </Col>
   <Col md={3}>
   Ads:
-  <br/>
+  {/* <Adsense
+  client="ca-pub-7640562161899788"
+  slot="7259870550"
+/> */}
+  {/* <br/>
   sdLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  </Col>
+   */}</Col>
 </Row>
         
 
