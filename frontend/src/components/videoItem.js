085ffd7352997a90faa582c133a2463dc7530c0e
Tyler Parks <tylerparks@my.unt.edu>
2021-04-13 15:11:44 -0500

    Added star button

--- frontend/src/components/videoItem.js
+++ frontend/src/components/videoItem.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Button, Card,  Container, Row, Col, CardDeck } from "react-bootstrap";
 import './video.scss';
+import './pages.scss';
+
+import {BiStar} from "react-icons/bi";
 
 const VideoItem = ({video , handleVideoSelect}) => {
+    const [count, setCount] = useState([]);
+    const [isVisible, setIsVisible] = useState(false);
+
+    var votes = 0;
+
+    const likeVideo = (votes) => {
+        console.log("Votes Before: " + votes);
+        votes+=1;
+        console.log("Votes After: " + votes);
+
+        //(isVisible) ?(votes -= 1):(votes += 1);
+        /* (isVisible) ? setCount(count - 1): setCount(count + 1); */
+        //setIsVisible(!isVisible);
+    };
+
     return (
         <>
             <div className='videoItem' onClick={ () => handleVideoSelect(video)}>
@@ -10,6 +29,16 @@ const VideoItem = ({video , handleVideoSelect}) => {
                     <h4>{video.snippet.title}</h4>
                     <div className='videoItemCreator'>{video.snippet.channelTitle}</div>
 
+                    {/* YouTube Video ID */}
+                    <h4>Video ID: {video.id.videoId}</h4>
+
+                    {/* Star Button */}
+                    <Button className="star-btn" variant="" onClick={() => likeVideo(votes) }>
+                        <BiStar className="star" color="gold" size= "2em"  />
+                    </Button>
+
+                    {/* Vote Count */}
+                    {votes}
                 </div>
             </div>
 
