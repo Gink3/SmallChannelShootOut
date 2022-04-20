15ca21ced861d277805ccac5eacaf6245d54004a
MichaelLMoser23 <Michaelmoser@my.unt.edu>
2022-04-20 18:28:13 -0500

    Update VideoSubmissionForm.js
    Added basic code that makes a simple form. Does nothing yet.

---
+++ frontend/src/components/VideoSubmissionForm.js
@@ -0,0 +1,38 @@
+//This is just a start I used this tutorial for reference:
+//https://www.pluralsight.com/guides/form-submission-in-reactjs
+
+
+import React from 'react';
+
+export default class FormSubmission extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchQuery: ""
+    }
+  }
+
+  handleInputChanged(event) {
+    this.setState({
+      searchQuery: event.target.value
+    });
+  }
+
+  handleButtonClicked() {
+    var searchQuery = this.state.searchQuery;
+
+    window.location.href = "" + searchQuery;    //after click button add redirect 
+  }
+
+  render() {
+    return  (
+      <div>
+        <input type="text" value={this.state.searchQuery} onChange={this.handleInputChanged.bind(this)}/>
+        <button onClick={this.handleButtonClicked.bind(this)}>
+          Submit
+        </button>
+      </div>
+    );
+  }
+}
