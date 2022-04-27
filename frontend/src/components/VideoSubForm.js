5ca8cda650fcb2c4aaba8f21f16b20a2ad0ddd57
MichaelLMoser23 <Michaelmoser@my.unt.edu>
2022-04-27 14:38:39 -0500

    Added data fields for form and database posts

---
+++ frontend/src/components/VideoSubForm.js
@@ -0,0 +1,126 @@
+import React, {useState, useContext} from 'react';
+import AuthContext from "../context/authcontext.js"
+import {Button, Form, Row, Col, Container} from "react-bootstrap";
+import "../style/signup.scss";
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+function VideoSubForm(){
+    const history = useHistory(); 
+    const {loggedIn} = useContext(AuthContext);
+    const[postVideo, setVideoData] = useState({
+        _id: "",  
+        videoId: "",
+        videoTitle: "", 
+        channelTitle: "",       //this is all the fields of data that are stored for videos
+        votes: "", 
+        videoThumbnail: "", 
+        videoPublishDate: "", 
+        _v: "",
+    }); 
+
+    const handleSubmit = event => {
+        event.preventDefault(); 
+
+        console.log("Handled submit");
+
+        axios.post('http://localhost:5000/', postVideo, {     //need to add route since component
+          withCredentials:true
+        }).then(function(res){
+            console.log(res);
+            history.push("/");
+            history.go(0);
+        }).catch(function(error) {
+            console.log(error);
+        }); 
+      
+    }
+
+    // Handles changes to input fields and updates
+    // postUser obj to later send to signup route
+    const handleChange = (event) => {
+        console.log(event);
+        setVideoData({
+            ...postVideo,
+            [event.target.name]: event.target.value
+        });
+    }
+
+        return (
+                 <>
+                 {loggedIn===false && (
+                <Container fluid>
+                   
+                    <Row className="justify-content-md-center " >
+
+                        <Col md={4} style={{backgroundColor:'black', marginTop:'2%', marginBottom:'2%', borderRadius:'9px'}}>
+                        <div className="d-flex justify-content-center header"> 
+                        <h2  style={{color:'white'}}>  Submit your video </h2>
+                         </div>
+                            <Form onSubmit={handleSubmit}>
+                                <br />
+                                <Form.Group controlId="_id">
+
+                                    <Form.Control required type="text" name="_id" className="input line" placeholder="ID" 
+                                    value={postVideo._id} onChange={handleChange}/>
+                                </Form.Group>
+
+                                <Form.Group controlId="videoId">
+
+                                    <Form.Control required type="text" name="videoId" className="line" placeholder="Video ID" 
+                                    value={postVideo.videoId} onChange={handleChange}/>
+                                </Form.Group>
+
+                                <Form.Group controlId="videoTitle">
+
+                                    <Form.Control required type="text" name="videoTitle" className="line" placeholder="Video Title" 
+                                    value={postVideo.videoTitle} onChange={handleChange}/>
+                                </Form.Group>
+
+                                <Form.Group controlId="channelTitle">
+
+                                    <Form.Control required type="text" name="channelTitle" className="line" placeholder="Channel Title" 
+                                    value={postVideo.channelTitle} onChange={handleChange}/>
+                                </Form.Group>
+
+                                <Form.Group controlId="votes">
+                                    <Form.Control required type="text" name="votes" className="line" placeholder="Votes"
+                                     value={postVideo.votes} onChange={handleChange}/>
+                                </Form.Group>
+
+
+                                <Form.Group controlId="videoThumbnail">
+                                    <Form.Control required type="text" name="videoThumbnail" className="line" placeholder="Video Thumbnail"
+                                     value = {postVideo.videoThumbnail} onChange={handleChange}/>
+                                </Form.Group>
+
+
+                                <Form.Group controlId="videoPublishDate">
+                                    <Form.Control required type="text" name="videoPublishDate" className="line" placeholder="Publish Date"
+                                     value = {postVideo.videoPublishDate} onChange={handleChange}/>
+                                </Form.Group>
+
+
+                                <Form.Group controlId="_v">
+                                    <Form.Control required type="text" name="_v" className="line" placeholder="V"
+                                     value = {postVideo._v} onChange={handleChange}/>
+                                </Form.Group>
+
+
+
+
+                                <br />
+                                <Button variant="" className="btn_createacc" size="lg" type="submit" block>
+                                    SIGN UP
+                                </Button>
+                                <br />
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
+               )} 
+            </>
+        );
+}
+
+export default VideoSubForm; 
